401b34c2b460b7e4ec9d14428b90cf1b1e7b3546
Geyson <geyson059@gmail.com>
2021-11-03 23:22:41 -0300

    Atualização Programação imperativa Aula07

--- Programação imperativa/Aula07 -27-11-2021/aula7.js
+++ Programação imperativa/Aula07 -27-11-2021/aula7.js
@@ -1,16 +1,50 @@
-!true
-!false
-!!false
-!!true
-!1
-!!1
-!0
-!!0
-!!"" 
-let x = 5 ;
-let y = 9;
-x < 10 && x!==5
-x>9 || x===5
-!(x===y)
+// 1 (false)
+console.log (!(true));
+// 2 (true)
+console.log (!(false));
+// 3 (false)
+console.log (!(!(false)));
+// 4 (true)
+console.log (!(!(true)));
+// 5 (false)
+console.log (!(1));
+// 6 (true)
+console.log (!(!(1)));
+// 7 (true)
+console.log (!(0));
+// 8 (false)
+console.log (!(!(0)));
+// 9 (false)
+console.log (!(!(""))); 
 
-console.log (!(true)))
+// 10 
+let x = 5;
+let y = 9;
+
+// a) (false)
+console.log (x < 10 && x!==5);
+// b) (true)
+console.log (x>9 || x===5);
+// c) (true)
+console.log (!(x===y));
+
+// 1 (true)
+let x1 = 10;
+let y1 = "a";
+
+console.log(y1==="b" || x1 >= 10);
+
+// 2 (false)
+let x2 = 3;
+let y2 = 8;
+
+console.log(!(x2 == "3" || x2 === y2) && !(y2 !== 8 && x2 <= y2));
+
+// 3 (false)
+let str = "";
+let msg = "haha!";
+let eBonito = "false";
+
+console.log(!((str || msg) && eBonito));
+
+// .
